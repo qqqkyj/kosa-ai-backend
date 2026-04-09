cf7b5e23fcd436dc5b69bbc5b612d2294e5cb76c
qqqkyj <123rkdduswn@naver.com>
2026-04-09 14:17:06 +0900

    init

---
+++ src/main/java/com/mycompany/backendapi/BackEndApiApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.backendapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mycompany/backendapi/BackEndApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.backendapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
