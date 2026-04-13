b7fef35323564c195af986d3a7f1632339e31e5e
qqqkyj <123rkdduswn@naver.com>
2026-04-13 12:29:41 +0900

    return-type(void - file input,output,image)

--- src/main/java/com/mycompany/backendapi/returntype/controller/ReturnTypeController.java
+++ src/main/java/com/mycompany/backendapi/returntype/controller/ReturnTypeController.java
@@ -1,5 +1,11 @@
 package com.mycompany.backendapi.returntype.controller;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
@@ -13,6 +19,7 @@ import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -100,7 +107,13 @@ public class ReturnTypeController {
 			//custom header 추가
 			HttpHeaders headers = new HttpHeaders();
 			headers.add("my-head", "my-value");
-			return new ResponseEntity<>(board, headers, HttpStatus.OK);
+			
+			//return new ResponseEntity<>(board, headers, HttpStatus.OK);
+			//builder 방식(static 메서드라 가능)
+			return ResponseEntity
+			        .status(HttpStatus.OK)
+			        .headers(headers)
+			        .body(board);
 			
 		} catch (Exception e) {
 			return new ResponseEntity<>(
@@ -129,13 +142,40 @@ public class ReturnTypeController {
 	//브라우저가 HTTP 요청을 보내면 Spring Boot 내장 Tomcat(WAS)이 이를 받아 처리
 	//Tomcat은 요청 정보를 담은 HttpServletRequest와 응답을 위한 HttpServletResponse 객체를 자동으로 생성
 	//그 후 Spring Controller가 이 객체들을 이용해 요청을 처리하고 응답을 만들어 반환
-	@GetMapping("/void")
-	public void download(HttpServletRequest request, HttpServletResponse response) {
+	@GetMapping("/void/{id}")
+	public void download(HttpServletRequest request, HttpServletResponse response, @PathVariable("id") int id) throws IOException{
 		log.info("실행");
 		//응답을 직접 생성
-		//1) Content-Type 설정: 응답 본문의 데이터 형식
-		//2) 응답 본문에 데이터를 넣기(출력)
+		//1) Content-Type 설정: 응답 본문(response body)의 데이터 형식
+		//Content-Type: image/jpeg
+		//response.addHeader("Content-Type", "image/jpeg");
+		response.setContentType("image/jpeg");
 		
+		//2) Body에 있는 데이터를 브라우저가 바로 보여주지 않고 파일 다운로드를 하도록 설정
+		String fileName = "사진" + id + ".jpg";
+		//java17 이전 버전은 한글 한글자를 2byte로 읽음(이전 버전 호환을 위해 추가)
+		fileName = new String(fileName.getBytes("UTF-8"), "ISO-8859-1");
+		response.addHeader("Content-Disposition", "attachment; filename=\"" + fileName + "\"");
+		
+		
+		//3) 응답 본문에 데이터를 넣기(출력)
+//		방식 1: 큰 파일(이미지 등)을 메모리 적게 쓰면서 다운로드/전송하는 방식
+//		InputStream is = new FileInputStream("C:\\kosa-course\\projects\\backend-spring\\back-end-api\\src\\main\\resources\\static\\images\\photo" + id +".jpg");
+//		OutputStream os = response.getOutputStream(); //응답 본문(response body)에 데이터를 출력
+//		
+//		byte[] buffer = new byte[1024]; //1k
+//		while(true) {
+//			int num = is.read(buffer); // 한 번에 buffer의 크기(1k)만큼 읽을 수 있음
+//			if(num == -1) break; // 다 읽으면 -1
+//			os.write(buffer, 0, num); //0인텍스에서 읽은 만큼만 출력
+//		}
+//		os.flush(); // 버퍼에 쌓여있는 데이터를 강제로 즉시 전송하는 기능
+//		is.close();
+//		os.close();
+		
+		//방식 2 : 파일 크기가 작은 경우
+		Files.copy(Path.of("C:\\kosa-course\\projects\\backend-spring\\back-end-api\\src\\main\\resources\\static\\images\\photo" + id +".jpg") ,
+				response.getOutputStream());
 	}
 	
 }
